884892f8207766150fd1b6fb91cbd1f82f05cff0
Lukas Jesche <ljaschka@gmail.com>
2021-08-01 22:25:28 +0200

    - [WIP] twitter worker finally doing stuff

--- src/twitter_worker.py
+++ src/twitter_worker.py
@@ -1,10 +1,24 @@
 import logging
+import re
+from datetime import date
 from multiprocessing.context import Process
+from difflib import SequenceMatcher
+import spacy
+from collections import Counter
 
 from event_stream.event_stream_consumer import EventStreamConsumer
 from event_stream.event_stream_producer import EventStreamProducer
 from event_stream.event import Event
 
+
+# compare text and return score
+def compare_text(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def normalize(string):
+    # todo numbers, special characters/languages
+    return (re.sub('[^a-zA-Z ]+', '', string)).casefold().strip()
 
 
 class TwitterWorker(EventStreamConsumer, EventStreamProducer):
@@ -13,7 +27,17 @@ class TwitterWorker(EventStreamConsumer, EventStreamProducer):
     publication_client = False
     log = "TwitterWorker "
     group_id = "twitter-worker"
+    nlp = False
 
+    source_score = {
+        'Twitter Web App': 10,
+        'Twitter for iPhone': 10,
+        'Twitter Web Android': 10,
+        'Twitter Web iPad': 10,
+        'Twitter Web Mac': 10,
+        'TweetDeck': 5,
+        'Twitter': 3,
+    }
 
     def on_message(self, json_msg):
         # print('hello')
@@ -24,23 +48,108 @@ class TwitterWorker(EventStreamConsumer, EventStreamProducer):
         e.from_json(json_msg)
 
         e.data['subj']['processed'] = {}
-        e.data['subj']['processed']['questionMarkCount'] = e.data['subj']['data']['text'].count("?")
-        e.data['subj']['processed']['exclamationMarkCount'] = e.data['subj']['data']['text'].count("!")
-        e.data['subj']['processed']['exclamationMarkCount'] = len(e.data['subj']['data']['text'])
+        e.data['subj']['processed']['question_mark_count'] = e.data['subj']['data']['text'].count("?")
+        e.data['subj']['processed']['exclamation_mark_count'] = e.data['subj']['data']['text'].count("!")
+        e.data['subj']['processed']['length'] = len(e.data['subj']['data']['text'])
+
+        split_date = e.data['obj']['data']['pubDate'].split('-')
+        pub_timestamp = date(split_date[0], split_date[1], split_date[2])
+
+        # todo use date from twitter not today
+        e.data['subj']['processed']['time_past'] = (date.today() - pub_timestamp).days
+
+        hashtags = []
+        annotations = []
+        a_types = []
+
+        if 'entities' in e.data['subj']['data']:
+            if 'hashtags' in e.data['subj']['data']['entities']:
+                for tag in e.data['subj']['data']['entities']['hashtags']:
+                    hashtags.append(normalize(tag['tag']))
+
+            # todo filter annotation types
+            if 'annotations' in e.data['subj']['data']['entities']:
+                for tag in e.data['subj']['data']['entities']['annotations']:
+                    annotations.append(tag['normalized_text'])
+                    a_types.append(tag['type'])
+
+        e.data['subj']['processed']['hashtags'] = hashtags
+        e.data['subj']['processed']['annotations'] = annotations
+        e.data['subj']['processed']['a_types'] = a_types
+
+        # todo filter context annotation types
+        context_a_domain = []
+        context_a_entity = []
+        if 'context_annotations' in e.data['subj']['data']:
+            for tag in e.data['subj']['data']['context_annotations']:
+                context_a_domain.append(tag['name'])
+                context_a_entity.append(tag['name'])
+        e.data['subj']['processed']['context_domain'] = context_a_domain
+        e.data['subj']['processed']['context_entity'] = context_a_entity
+
+        # containsAbstract, calculate score for comparison
+        e.data['subj']['processed']['contains_abstract'] = compare_text(e.data['subj']['data']['text'],
+                                                                        e.data['obj']['data']['abstract'])
+        # isBot
+        if e.data['subj']['data']['source'] in self.source_score:
+            e.data['subj']['processed']['bot_rating'] = self.source_score[e.data['subj']['data']['source']]
+        else:
+            e.data['subj']['processed']['bot_rating'] = 1
+
+        # typeOfTweet (quote, retweet, orginal)
+        if not e.data['subj']['data']['referenced_tweets']['type']:
+            e.data['subj']['processed']['tweet_type'] = 'tweet'
+        else:
+            e.data['subj']['processed']['tweet_type'] = e.data['subj']['data']['referenced_tweets'][0]['type']
+
+        # use nltk and check for lang = eng, stop words etc
+        # normalized tokenized top words from text, remove hashtags and stuff ?
+        text = e.data['subj']['data']['text'].strip().lower()
+        e.data['subj']['processed']['words'] = self.spacy_process(text)
 
         # - Sentiment
-        # - time between tweet and pubDate
-        # pubDate = e.data['obj']['']
-        # - hashtags (semantic normalized?)
-        # - entities (semantic normalized?)
-        # - containsAbstract
-        # - isBot
-        # - typeOfTweet (quote, retweet, orginal)
         # - score (how high we rank this tweet)
-        # - match tweet author and publication author
+        # - match tweet author and publication author # compare just with text compare
 
         e.set('state', 'processed')
         self.publish(e)
+
+
+    # https://towardsdatascience.com/text-normalization-with-spacy-and-nltk-1302ff430119
+    def spacy_process(self, text):
+        if not self.nlp:
+            self.nlp = spacy.load('en_core_web_sm')
+
+        doc = self.nlp(text)
+
+        # Tokenization and lemmatization are done with the spacy nlp pipeline commands
+        lemma_list = []
+        for token in doc:
+            lemma_list.append(token.lemma_)
+        # print("Tokenize+Lemmatize:")
+        # print(lemma_list)
+
+        # Filter the stopword
+        filtered_sentence = []
+        for word in lemma_list:
+            lexeme = self.nlp.vocab[word]
+            if lexeme.is_stop == False:
+                filtered_sentence.append(word)
+
+                # Remove punctuation
+        punctuations = "?:!.,;"
+        for word in filtered_sentence:
+            if word in punctuations:
+                filtered_sentence.remove(word)
+        # print(" ")
+        # print("Remove stopword & punctuation: ")
+        # print(filtered_sentence)
+
+        # todo check if this makes sense, we may have no duplicates here
+        word_freq = Counter(filtered_sentence)
+        common_words = word_freq.most_common(10)
+        return common_words
+
 
 # from kafka import KafkaConsumer, KafkaProducer
 #
